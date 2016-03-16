58666590094de4013842a8a98405e88c72af5648
Aaron Botello <aaronbotrod@gmail.com>
2016-03-15 22:35:38 -0700

    Added support for PR

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ var Repo = require( "git-tools" );
 var repo = new Repo( "." );
 var open = require("open");
 var URL = require('url-parse');
+var commandLineArgs = require('command-line-args');
 
+var cla = new commandLineArgs([{ name: 'pr_branch', type: String, defaultOption: true }, { name: 'debug', type: Boolean, alias: 'd', defaultValue : false}]);
 repo.currentBranch(function( error, branch ) {
 
   repo.remotes(function( error, remotes ) {
-
+    var options = cla.parse();
+    if(options.debug) {
+      console.log('options', options);
+    }
+    
     var url = new URL(remotes[0].url);
     if(url.auth === 'git') {
       var hostNameAndusername = url.host.split(':');
@@ -22,11 +28,20 @@ repo.currentBranch(function( error, branch ) {
       var hostName = url.host;
     }
 
-    url.set('pathname', '/'+username+'/'+repoName+'/tree/'+branch);
     url.set('protocol', 'https:');
     url.set('hostname', hostName);
+    if(options.pr_branch) {
+      url.set('pathname', '/'+username+'/'+repoName+'/compare/'+options.pr_branch+"..."+branch);
+    } else {
+      url.set('pathname', '/'+username+'/'+repoName+'/tree/'+branch);
+    }
     
-    open(url.toString());
+
+    console.log("Openning "+url.toString());
+    if(!options.debug) {
+      open(url.toString());  
+    }
+    
   
     });
 });
