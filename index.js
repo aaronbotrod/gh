e07d8f64882deef961ad24b89e20bf59274aaf35
Aaron Botello <aaronbotrod@gmail.com>
2016-03-15 22:11:20 -0700

    Adding support for ssh origin urls

--- index.js
+++ index.js
@@ -10,13 +10,24 @@ repo.currentBranch(function( error, branch ) {
   repo.remotes(function( error, remotes ) {
 
     var url = new URL(remotes[0].url);
-    console.log(url.pathname);
-    var username = url.pathname.split('/')[1];
-    var repoName = url.pathname.split('/')[2].split('.')[0];
+    if(url.auth === 'git') {
+      var hostNameAndusername = url.host.split(':');
+      var hostName = hostNameAndusername[0];
+      var username = hostNameAndusername[1];
+      var repoName = url.pathname.split('/')[1].split('.')[0];
+      
+    } else {
+      var username = url.pathname.split('/')[1];
+      var repoName = url.pathname.split('/')[2].split('.')[0];
+      var hostName = url.host;
+    }
 
-    url.set('protocol', 'https:');
     url.set('pathname', '/'+username+'/'+repoName+'/tree/'+branch);
+    url.set('protocol', 'https:');
+    url.set('hostname', hostName);
+    
     open(url.toString());
-  });
+  
+    });
 });
 
